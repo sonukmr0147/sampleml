72cb0fd1b60046ae5b79145abbbf08ceb7bbd511
sonu kumar <sonukmr0147@gmail.com>
2025-08-26 09:40:09 +0530

    update in data

--- src/exception.py
+++ src/exception.py
@@ -2,13 +2,14 @@ import sys
 #import traceback
 
 def error_message_details(error,error_detail:sys):
-    _, _, exec_tb=error_details.exec_info()
+    _, _, exec_tb=error_detail.exec_info()
     file_name=exec_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name[{0}] line number [{1}] error meassage [{2}]".format(
         file_name,exec_tb.tb_lineno, str(error)
     )
 
 class CustomException(Exception):
+    
     def __init__(self,error_message, error_detail: sys):
         super.__init__(error_message)
         self.error_message=error_message_details(error_message , error_details=error_detail)
